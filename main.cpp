c4ab07590a0410b5e7f3378108a2a7389d6004d8
Yaroslav Boyko <yaroslav.bojko.82@bk.ru>
2023-12-01 18:21:10 +0300

    finish

--- main.cpp
+++ main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    int a = 42;
+    int b = 153;
+
+    std::cout << "a = " << a << std::endl;
+    std::cout << "b = " << b << std::endl;
+    std::cout << "--------" << std::endl;
+
+    a = a + b;
+    b = a - b;
+    a = a - b;
+
+    std::cout << "a = " << a << std::endl;
+    std::cout << "b = " << b << std::endl;
 }
